00f219e4df4606b890db96b29bf574f37e28a3c4
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2024-08-21 15:48:31 +0530

    Fixed alignment issues

--- container/index.ts
+++ container/index.ts
@@ -43,4 +43,4 @@ export { default as Works } from './workiz-page/Works';
 export { default as Chelenge } from './workiz-page/Chelenge';
 export { default as Result } from './workiz-page/Result';
 export { default as Credit } from './workiz-page/Credit';
-export { default as VideoWorkiz } from './workiz-page/Video';
+export { default as VideoWorkiz } from './workiz-page/Video';// Update on 08/21/2024 15:48:31
