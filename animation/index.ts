519ab4a2341bb04f9596ed6b28f4182ab0bc3fdc
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2024-09-08 01:02:22 +0530

    Refactored CSS for better performance

--- animation/index.ts
+++ animation/index.ts
@@ -2,3 +2,4 @@ export { default as TextHover } from './TextHover';
 export { default as TextMarquee } from './LogoMarquee';
 export { default as TextMask } from './TextMask';
 export { default as LinkHover } from './LinkHover';// Update on 08/14/2024 16:26:49
+// Update on 09/08/2024 01:02:22
