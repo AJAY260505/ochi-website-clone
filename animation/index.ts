c1539e2fd0e0fa7dec089a2cbc91072fedfb80f5
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2024-10-12 08:14:11 +0530

    Updated documentation

--- animation/index.ts
+++ animation/index.ts
@@ -3,3 +3,4 @@ export { default as TextMarquee } from './LogoMarquee';
 export { default as TextMask } from './TextMask';
 export { default as LinkHover } from './LinkHover';// Update on 08/14/2024 16:26:49
 // Update on 09/08/2024 01:02:22
+// Update on 10/12/2024 08:14:11
