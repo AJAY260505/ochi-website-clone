0f56e6e8bd1734df180bcfb878e6cff3de9d8e39
AJAY J <120657267+AJAY260505@users.noreply.github.com>
2025-01-01 04:28:44 +0530

    Fixed alignment issues

--- animation/index.ts
+++ animation/index.ts
@@ -4,3 +4,4 @@ export { default as TextMask } from './TextMask';
 export { default as LinkHover } from './LinkHover';// Update on 08/14/2024 16:26:49
 // Update on 09/08/2024 01:02:22
 // Update on 10/12/2024 08:14:11
+// Update on 01/01/2025 04:28:44
